ce7d08e9160968cb837a9e92dce3c417fe0541e3
elpiolito <github@JLP-1988>
2017-04-05 15:31:53 +0200

    Change - Footer design
    Adpat footer design

--- footer.php
+++ footer.php
@@ -1,26 +1,29 @@
 <?php if (!defined('PLX_ROOT')) exit; ?>
 
-		<footer class="footer" role="contentinfo">
+<footer class="site-footer" role="contentinfo">
 
-				<p>
-					&copy; 2016 <?php $plxShow->mainTitle('link'); ?> -
-					<?php $plxShow->subTitle(); ?> -
-					<?php $plxShow->lang('POWERED_BY') ?>&nbsp;<a href="http://www.pluxml.org" title="<?php $plxShow->lang('PLUXML_DESCRIPTION') ?>">PluXml</a>
-					<?php $plxShow->lang('IN') ?>&nbsp;<?php $plxShow->chrono(); ?>&nbsp;
-					<?php $plxShow->httpEncoding() ?>
-				</p>
-				<ul class="menu">
-					<li><a href="<?php $plxShow->racine() ?>"><?php $plxShow->lang('HOME'); ?></a></li>
-					<li><a href="<?php $plxShow->urlRewrite('#top') ?>" title="<?php $plxShow->lang('GOTO_TOP') ?>"><?php $plxShow->lang('TOP') ?></a></li>
-					<li><a rel="nofollow" href="<?php $plxShow->urlRewrite('core/admin/'); ?>" title="<?php $plxShow->lang('ADMINISTRATION') ?>"><?php $plxShow->lang('ADMINISTRATION') ?></a></li>
-				</ul>
-
-		</footer>
-
-
+    <div class="site-info small-part">
+        &copy; 2016 <?php $plxShow->mainTitle('link'); ?> -
+        <?php $plxShow->subTitle(); ?> -
+        <?php $plxShow->lang('POWERED_BY') ?>&nbsp;<a href="http://www.pluxml.org" title="<?php $plxShow->lang('PLUXML_DESCRIPTION') ?>">PluXml</a>
+        <?php $plxShow->lang('IN') ?>&nbsp;<?php $plxShow->chrono(); ?>&nbsp;
+        <?php $plxShow->httpEncoding() ?>
     </div>
 
+</footer>
+
+
 </div>
+
+</div>
+
+<a href="#top" class="to-top"><i class="fa fa-angle-up fa-lg"><span class="screen-reader-text">Back to top</span></i></a>
+
+<script src="js/jquery-3.2.0.min.js" type="text/javascript" ></script>
+<script src="js/html5shiv-printshiv.min.js" type="text/javascript" ></script>
+<script src="js/modernizr.custom.71422.js" type="text/javascript" ></script>
+<script src="js/skip-link-focus-fix.js" type="text/javascript" ></script>
+<script src="js/nucleare.js.js" type="text/javascript" ></script>
 
 </body>
 
